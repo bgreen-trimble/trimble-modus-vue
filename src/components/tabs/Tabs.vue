6bd78bba178076c42f4bd396aac5bf895be6d4a5
Bruce Green <bruce_green@trimble.com>
2025-06-04 00:46:19 -0500

    2025-06-04-03

--- src/components/tabs/Tabs.vue
+++ src/components/tabs/Tabs.vue
@@ -3,7 +3,7 @@ import { computed, provide, ref, onMounted, watch, nextTick } from 'vue'
 import type { TabItem } from './types'
 
 export interface TabsProps {
-  activeId: string  // Changed from modelValue
+  activeId: string 
   size?: 'small' | 'medium' | 'large'
   variant?: 'default' | 'bordered' | 'borderless'
   vertical?: boolean
@@ -16,7 +16,7 @@ const props = withDefaults(defineProps<TabsProps>(), {
 })
 
 const emit = defineEmits<{
-  'update:activeId': [value: string]  // Changed from update:modelValue
+  'update:activeId': [value: string]
 }>()
 
 const tabs = ref<TabItem[]>([])
@@ -208,139 +208,4 @@ const handleKeyNavigation = (event: KeyboardEvent) => {
 
 <style scoped>
 @import './tabs.css';
-
-.tm-tabs {
-  display: flex;
-  flex-direction: column;
-}
-
-.tm-tabs-vertical {
-  flex-direction: row;
-}
-
-.tm-tabs-nav {
-  position: relative;
-  display: flex;
-  border-bottom: 1px solid var(--color-tm-tab-border);
-}
-
-.tm-tabs-nav-vertical {
-  flex-direction: column;
-  border-bottom: none;
-  border-right: 1px solid var(--color-tm-tab-border);
-}
-
-.tm-tab-button {
-  position: relative;
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
-  padding: var(--tm-tab-padding-y) var(--tm-tab-padding-x);
-  border: none;
-  background: none;
-  color: var(--color-tm-tab-text);
-  cursor: pointer;
-  transition: color 0.2s, background-color 0.2s;
-}
-
-.tm-tab-button:hover:not(.tm-tab-disabled) {
-  color: var(--color-tm-tab-text-hover);
-  background-color: var(--color-tm-tab-hover-bg);
-}
-
-.tm-tab-active {
-  color: var(--color-tm-tab-text-active) !important;
-}
-
-.tm-tab-disabled {
-  color: var(--color-tm-tab-text-disabled);
-  cursor: not-allowed;
-}
-
-.tm-tabs-active-indicator {
-  position: absolute;
-  background-color: var(--color-tm-tab-active-border);
-  transition: all 0.3s ease;
-}
-
-.tm-tabs-nav:not(.tm-tabs-nav-vertical) .tm-tabs-active-indicator {
-  bottom: -1px;
-  height: 2px;
-}
-
-.tm-tabs-nav-vertical .tm-tabs-active-indicator {
-  right: -1px;
-  width: 2px;
-}
-
-/* Size variants */
-.tm-tabs-small .tm-tab-button {
-  height: var(--tm-tab-height-small);
-  font-size: 0.875rem;
-}
-
-.tm-tabs-medium .tm-tab-button {
-  height: var(--tm-tab-height-medium);
-  font-size: 1rem;
-}
-
-.tm-tabs-large .tm-tab-button {
-  height: var(--tm-tab-height-large);
-  font-size: 1.125rem;
-}
-
-/* Bordered variant */
-.tm-tabs-bordered .tm-tab-button {
-  margin-bottom: -1px;
-  border: 1px solid transparent;
-}
-
-.tm-tabs-bordered .tm-tab-active {
-  border-color: var(--color-tm-tab-border);
-  border-bottom-color: transparent;
-  background-color: var(--color-tm-white);
-}
-
-.tm-tabs-bordered.tm-tabs-vertical .tm-tab-button {
-  margin-bottom: 0;
-  margin-right: -1px;
-}
-
-.tm-tabs-bordered.tm-tabs-vertical .tm-tab-active {
-  border-color: var(--color-tm-tab-border);
-  border-right-color: transparent;
-}
-
-.tm-tabs-content {
-  padding: 1rem;
-  flex: 1;
-}
-
-/* Focus styles */
-.tm-tab-button:focus-visible {
-  outline: none;
-  box-shadow: inset 0 0 0 2px var(--color-tm-tab-focus-ring);
-}
-
-.tm-tab-icon {
-  display: inline-flex;
-  align-items: center;
-  margin-right: 0.5rem;
-  vertical-align: middle;
-}
-
-.tm-tab-icon i {
-  font-size: 1.25em;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.tm-tab-label {
-  vertical-align: middle;
-}
-
-.tm-tabs-vertical .tm-tab-button {
-  justify-content: flex-start;
-}
 </style>
